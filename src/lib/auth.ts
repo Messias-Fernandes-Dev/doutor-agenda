71cf0f6efda5df2ec602b1a88eae00d7dfd0b68c
Messias Fernandes <dev.messiasfer@gmail.com>
2026-01-28 18:55:23 -0300

    fix: enhance layout styling, improve sidebar item activation logic, and ensure safe access to clinic information in authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,11 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { customSession } from "better-auth/plugins"
+import { customSession } from "better-auth/plugins";
 import { eq } from "drizzle-orm";
 
 import { db } from "@/db";
-import * as schema from "@/db/schema"
+import * as schema from "@/db/schema";
+import { usersToClinicsTable } from "@/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { 
@@ -19,21 +20,22 @@ export const auth = betterAuth({
   },
   },
   plugins: [
-    customSession(async ({user, session}) => {
+    customSession(async ({ user, session }) => {
       const clinics = await db.query.usersToClinicsTable.findMany({
-        where: eq(schema.usersToClinicsTable.userId, user.id),
+        where: eq(usersToClinicsTable.userId, user.id),
         with: {
           clinic: true,
-        }
+        },
       })
-      const clinic = clinics[0]
+      const clinic = clinics?.[0];
       return{
         user: {
           ...user,
-          clinic: {
-            id: clinic.clinicId,
-            name: clinic.clinic.name,
+          clinic: clinic?.clinicId? {
+            id: clinic?.clinicId,
+            name: clinic?.clinic?.name,
           }
+          :undefined
         },
         session,
       }
