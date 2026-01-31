aa280b2392fdaa3bd8426afcdd71f1a91c6c2438
Messias Fernandes <dev.messiasfer@gmail.com>
2026-01-31 20:33:13 -0300

    feat: add next-safe-action for improved form handling and update add-doctor-button and upsert-doctor-form components for better state management

--- src/app/(protected)/doctors/_components/add-doctor-button.tsx
+++ src/app/(protected)/doctors/_components/add-doctor-button.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Plus } from "lucide-react"
+import { useState } from "react"
 
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogTrigger } from "@/components/ui/dialog"
@@ -9,15 +10,16 @@ import UpsertDoctorForm from "./upsert-doctor-form"
 
 
 const AddDoctorButton = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
         <Button>
           <Plus />
           Adicionar médicos
         </Button>
       </DialogTrigger>
-      <UpsertDoctorForm />
+      <UpsertDoctorForm onSuccess={() => setIsOpen(false)} />
     </Dialog>
 
   )
